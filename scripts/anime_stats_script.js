9e828411d93fe84b24375bd3cf04bf9b943d2573
GDjkhp <jkhp.jkpa@gmail.com>
2023-02-18 20:44:09 +0800

    this is all coming together
    - new logo
    - added mal stats
    - commentary
    - optimizations

---
+++ scripts/anime_stats_script.js
@@ -0,0 +1,26 @@
+async function getStats() {
+	// https://api.jikan.moe/v4/users/{username}/statistics
+
+	const result = await fetch(`https://api.jikan.moe/v4/users/GDjkhp/statistics`).then(res => res.json());
+	const data = result.data.anime;
+	console.log(data);
+	
+	var stat = document.getElementsByClassName('stats');
+	
+	stat[0].style = "background-color: #2db039;";
+	stat[0].innerHTML = "Watching: " + data.watching;
+	
+	stat[1].style = "background-color: #26448f;";
+	stat[1].innerHTML = " Completed: " + data.completed;
+	
+	stat[2].style = "background-color: #f9d457;";
+	stat[2].innerHTML = " On-Hold: " + data.on_hold;
+	
+	stat[3].style = "background-color: #a12f31;";
+	stat[3].innerHTML = " Dropped: " + data.dropped;
+	
+	stat[4].style = "background-color: #c3c3c3;";
+	stat[4].innerHTML = " Plan to Watch: " + data.plan_to_watch;
+}
+
+getStats();
--- scripts/utility.js
+++ scripts/utility.js
@@ -3,9 +3,7 @@ var _today = new Date();
 //age
 var age = document.getElementById('age');
 
-if (strftime("%b", _today) != "Jan") 
-	age.innerHTML = strftime("%Y", _today) - 2003;
-else if (strftime("%e", _today) < 29)
+if (strftime("%b", _today) == "Jan" && strftime("%e", _today) < 29) 
 	age.innerHTML = strftime("%Y", _today) - 2004;
 else
 	age.innerHTML = strftime("%Y", _today) - 2003;

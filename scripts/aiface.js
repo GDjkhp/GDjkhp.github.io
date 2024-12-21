f69dc4b96eadd29f195a4a7da2a66c8ab2284a5f
GDjkhp <jkhp.jkpa@gmail.com>
2024-12-21 10:39:55 +0800

    DOMContentLoaded

--- scripts/aiface.js
+++ scripts/aiface.js
@@ -1,37 +1,39 @@
-var style = document.createElement("style");
-style.innerHTML = `
-#canvasdonut2 { 
-    position: fixed;
-	z-index: -1;
-    left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
-} 
-.markdown-body {
-    position: absolute;
-    left: 0;
-    right: 0;
-    z-index: 1;
-}
-body { 
-    background-color: black; 
-    color: white; 
-}
-tr, th, td {
-    background-color: black;
-}
-`;
-document.head.appendChild(style);
+document.addEventListener('DOMContentLoaded', function() {
+    var style = document.createElement("style");
+    style.innerHTML = `
+    #canvasdonut2 { 
+        position: fixed;
+        z-index: -1;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    } 
+    .markdown-body {
+        position: absolute;
+        left: 0;
+        right: 0;
+        z-index: 1;
+    }
+    body { 
+        background-color: black; 
+        color: white; 
+    }
+    tr, th, td {
+        background-color: black;
+    }
+    `;
+    document.head.appendChild(style);
 
-var donutCanvas = document.createElement("canvas");
-donutCanvas.setAttribute("id", "canvasdonut2");
-donutCanvas.setAttribute("width", "1500");
-donutCanvas.setAttribute("height", "1500");
-document.body.appendChild(donutCanvas);
+    var donutCanvas = document.createElement("canvas");
+    donutCanvas.setAttribute("id", "canvasdonut2");
+    donutCanvas.setAttribute("width", "1500");
+    donutCanvas.setAttribute("height", "1500");
+    document.body.appendChild(donutCanvas);
 
-var donutScript = document.createElement("script");
-donutScript.setAttribute("src", "https://GDjkhp.github.io/scripts/donut.js");
-donutScript.onload = function() {
-    anim2();
-};
-document.body.appendChild(donutScript);
+    var donutScript = document.createElement("script");
+    donutScript.setAttribute("src", "https://GDjkhp.github.io/scripts/donut.js");
+    donutScript.onload = function() {
+        anim2();
+    };
+    document.body.appendChild(donutScript);
+});

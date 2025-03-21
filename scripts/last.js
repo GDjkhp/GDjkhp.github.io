b4ca749d66f0a1565d822c42138e11d79523d378
GDjkhp <jkhp.jkpa@gmail.com>
2025-03-21 16:59:03 +0800

    more last shit

--- scripts/last.js
+++ scripts/last.js
@@ -1,18 +1,28 @@
 async function main() {
-    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=GDjkhp&period=12month&limit=1000&api_key=a564a411fff98ed4362d09de1af9710f&format=json`;
-    data = await fetch(url).then(res => res.json());
-    console.log(data);
-    console.log(data.toptracks.track);
+    user = "GDjkhp"
+    period = "12month"
+    limit = "1000"
+    api = "a564a411fff98ed4362d09de1af9710f";
 
-    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=GDjkhp&period=12month&limit=1000&api_key=a564a411fff98ed4362d09de1af9710f&format=json`;
-    data = await fetch(url).then(res => res.json());
-    console.log(data);
-    console.log(data.topalbums.album);
+    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
+    // data = await fetch(url).then(res => res.json());
+    // console.log(data);
+    // console.log(data.toptracks.track);
 
-    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=GDjkhp&period=12month&limit=1000&api_key=a564a411fff98ed4362d09de1af9710f&format=json`;
+    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
+    // data = await fetch(url).then(res => res.json());
+    // console.log(data);
+    // console.log(data.topalbums.album);
+
+    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
+    // data = await fetch(url).then(res => res.json());
+    // console.log(data);
+    // console.log(data.topartists.artist);
+
+    var url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
     data = await fetch(url).then(res => res.json());
     console.log(data);
-    console.log(data.topartists.artist);
+    console.log(data.recenttracks.track);
 }
 
 main();

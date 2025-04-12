fc55102acb644d27c9a5f74e6c52feee83b3e734
GDjkhp <jkhp.jkpa@gmail.com>
2025-04-12 22:55:24 +0800

    scrobbling paging

--- scripts/last.js
+++ scripts/last.js
@@ -22,51 +22,57 @@ function delay(ms) {
     });
 }
 
+async function morefm() {
+    trackpage++;
+
+    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${trackuser}&period=${trackperiod}&page=${trackpage}&limit=${tracklimit}&api_key=${trackapi}&format=json`;
+    data = await fetch(url).then(res => res.json());
+    console.log(data);
+    console.log(data.toptracks.track);
+
+    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${trackuser}&period=${trackperiod}&page=${trackpage}&limit=${tracklimit}&api_key=${trackapi}&format=json`;
+    data = await fetch(url).then(res => res.json());
+    console.log(data);
+    console.log(data.topalbums.album);
+
+    var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${trackuser}&period=${trackperiod}&page=${trackpage}&limit=${tracklimit}&api_key=${trackapi}&format=json`;
+    data = await fetch(url).then(res => res.json());
+    console.log(data);
+    console.log(data.topartists.artist);
+}
+
+let trackpage = 0;
+let tracklimit = 100;
+let trackuser = "GDjkhp";
+let trackperiod = "12month";
+let trackapi = "a564a411fff98ed4362d09de1af9710f";
+
 async function mainfm() {
-    user = "GDjkhp"
-    period = "12month"
-    limit = "1000"
-    api = "a564a411fff98ed4362d09de1af9710f";
-
-    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
-    // data = await fetch(url).then(res => res.json());
-    // console.log(data);
-    // console.log(data.toptracks.track);
-
-    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
-    // data = await fetch(url).then(res => res.json());
-    // console.log(data);
-    // console.log(data.topalbums.album);
-
-    // var url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
-    // data = await fetch(url).then(res => res.json());
-    // console.log(data);
-    // console.log(data.topartists.artist);
-
-    var url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&period=${period}&limit=${limit}&api_key=${api}&format=json`;
+    trackpage++;
+    var url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${trackuser}&period=${trackperiod}&page=${trackpage}&limit=${tracklimit}&api_key=${trackapi}&format=json`;
     data = await fetch(url).then(res => res.json());
     // console.log(data);
     // console.log(data.recenttracks.track);
 
-    number = 0;
-    data.recenttracks.track.forEach(async t => {
+    let tracknumber = document.getElementsByClassName("track").length;
+    data.recenttracks.track.forEach(async trackdata => {
+        if (trackdata["@attr"]) return;
         song_template();
-        let img   = document.getElementsByClassName("track-cover") [number];
-        let title = document.getElementsByClassName("track-title") [number];
-        let artist= document.getElementsByClassName("track-artist")[number];
-        let album = document.getElementsByClassName("track-album") [number];
-        let time  = document.getElementsByClassName("track-date")  [number];
-        let link  = document.getElementsByClassName("track-link")  [number];
-        number++;
-        img.src = t.image[3]["#text"];
-        title.innerHTML = t.name;
-        link.href = t.url;
-        artist.innerHTML = t.artist["#text"];
-        album.innerHTML = t.album["#text"];
-        time.innerHTML = t["@attr"] ? "Now playing" : strftime("%a %b %e %r %Y %Z", new Date(t.date["uts"]*1000));
+        let img   = document.getElementsByClassName("track-cover") [tracknumber];
+        let title = document.getElementsByClassName("track-title") [tracknumber];
+        let artist= document.getElementsByClassName("track-artist")[tracknumber];
+        let album = document.getElementsByClassName("track-album") [tracknumber];
+        let time  = document.getElementsByClassName("track-date")  [tracknumber];
+        let link  = document.getElementsByClassName("track-link")  [tracknumber];
+        tracknumber++;
+        img.src = trackdata.image[3]["#text"];
+        title.innerHTML = trackdata.name;
+        link.href = trackdata.url;
+        artist.innerHTML = trackdata.artist["#text"];
+        album.innerHTML = trackdata.album["#text"];
+        time.innerHTML = strftime("%a %b %e %r %Y %Z", new Date(trackdata.date["uts"]*1000));
     });
 	createScrollAnimation('track');
 	window.addEventListener('resize', createScrollAnimation('track'));
 }
-
 mainfm();
